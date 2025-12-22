c5952f37bae153c6f933ff31254aef7a9a9539b5
Avichai <avichai.shchori@gmail.com>
2025-12-22 10:16:23 +0200

    Clean index.ts

--- homework-3/src/components/index.ts
+++ homework-3/src/components/index.ts
@@ -1,9 +1,4 @@
-import Header from "./Header";
-import CartSidebar from "./Cart";
-import ToastHost from "./ToastHost";
-import GlobalLoadingIndicator from "./GlobalLoadingIndicator";
-
-export { Header };
-export { CartSidebar };
-export { ToastHost };
-export { GlobalLoadingIndicator };
+export * from './Header';
+export * from './Cart';
+export * from './ToastHost';
+export * from './GlobalLoadingIndicator';
